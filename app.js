751f5a701f0f053576b5a9a1cd92ecc9ac0044e2
Daam Ricardi <damianricardi96@gmail.com>
2023-12-24 15:30:06 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+
 const ProductManager = require('./productManager');
+
 
 const productManager = new ProductManager();
 
+
 console.log(productManager.getProducts());
+
 
 const newProduct = productManager.addProduct(
   "producto prueba",
@@ -16,21 +20,31 @@ console.log("Producto agregado:", newProduct);
 
 console.log(productManager.getProducts());
 
-try {
-  productManager.addProduct(
-    "producto prueba",
-    "Este es un producto prueba",
-    200,
-    "Sin imagen",
-    "abc123",
-    25
-  );
-} catch (error) {
-  console.error(error.message);
-}
 
 try {
   const foundProduct = productManager.getProductById(newProduct.id);
   console.log("Producto encontrado por ID:", foundProduct);
 } catch (error) {
   console.error(error.message);
+}
+
+
+try {
+  const updatedProduct = productManager.updateProduct(newProduct.id, {
+    price: 250,
+    stock: 30
+  });
+  console.log("Producto actualizado:", updatedProduct);
+} catch (error) {
+  console.error(error.message);
+}
+
+
+try {
+  const deletedProduct = productManager.deleteProduct(newProduct.id);
+  console.log("Producto eliminado:", deletedProduct);
+} catch (error) {
+  console.error(error.message);
+}
+
+console.log(productManager.getProducts());
--- productManager.js
+++ productManager.js
@@ -8,12 +8,15 @@ class ProductManager {
     }
   
     addProduct(title, description, price, thumbnail, code, stock) {
+      
       if (this.products.some(product => product.code === code)) {
         throw new Error("Error: El código del producto ya está en uso.");
       }
   
+     
       const id = Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
   
+      
       const product = {
         id,
         title,
@@ -24,7 +27,7 @@ class ProductManager {
         stock
       };
   
-
+     
       this.products.push(product);
   
       return product;
@@ -38,6 +41,35 @@ class ProductManager {
       }
   
       return product;
+    }
+  
+    updateProduct(productId, updatedFields) {
+      const productIndex = this.products.findIndex(product => product.id === productId);
+  
+      if (productIndex === -1) {
+        throw new Error("Error: Producto no encontrado para actualizar.");
+      }
+  
+     
+      updatedFields.id = productId;
+  
+      
+      Object.assign(this.products[productIndex], updatedFields);
+  
+      return this.products[productIndex];
+    }
+  
+    deleteProduct(productId) {
+      const productIndex = this.products.findIndex(product => product.id === productId);
+  
+      if (productIndex === -1) {
+        throw new Error("Error: Producto no encontrado para eliminar.");
+      }
+  
+      
+      const deletedProduct = this.products.splice(productIndex, 1)[0];
+  
+      return deletedProduct;
     }
   }
   
